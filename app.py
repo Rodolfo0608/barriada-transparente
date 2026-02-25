336a64ee525919d3ba22e9e485ccfd5a152b2be3
Rodolfo0608 <rodolfourriola@gmail.com>
2026-02-25 14:38:26 -0500

    Corrección para conectar a la base de datos de Supabase

--- app.py
+++ app.py
@@ -647,4 +647,8 @@ def logout():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
+#if __name__ == '__main__':
+#    app.run(debug=True)
